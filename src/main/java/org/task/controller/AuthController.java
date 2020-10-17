fd37ffc66f8910f976775c35ab2fe1ac23db699c
WaifuAnton <WaifuAnton@users.noreply.github.com>
2020-10-17 16:31:34 +0300

    Normal auth

---
+++ src/main/java/org/task/controller/AuthController.java
@@ -0,0 +1,21 @@
+package org.task.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.task.service.AuthService;
+
+@RestController
+public class AuthController {
+    private AuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/auth")
+    public String auth() {
+        return authService.getToken();
+    }
+}
---
+++ src/main/java/org/task/service/AuthService.java
@@ -0,0 +1,27 @@
+package org.task.service;
+
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class AuthService {
+    public String getToken() {
+        String link = "http://interview.agileengine.com/auth";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        Map<String, Object> map = new HashMap<>();
+        map.put("apiKey", "23567b218376f79d9415");
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(map, headers);
+        RestTemplate template = new RestTemplate();
+        ResponseEntity<String> response = template.postForEntity(link, entity, String.class);
+        if (response.getStatusCode() == HttpStatus.OK)
+            return response.getBody();
+        return null;
+    }
+}
